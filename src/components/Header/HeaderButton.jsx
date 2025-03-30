1bc838fb17696e63b43fe642abd9a6efbec712ac
Vytrx <Vytrx13@gmail.com>
2025-03-30 15:38:15 -0300

    mudei HeaderButton p usar styled-components

--- src/components/Header/HeaderButton.jsx
+++ src/components/Header/HeaderButton.jsx
@@ -1,12 +1,31 @@
 import './HeaderButton.css';
+import { styled } from 'styled-components';
+
+const Button = styled.button`
+    background-color: white;
+    color: #3586ff;
+    border: none;
+    padding: 10px 18px;
+    border-radius: 8px;
+    font-size: 1rem;
+    font-family: "Roboto", sans-serif;
+    cursor: pointer;
+    transition: background-color 0.3s ease, color 0.3s ease;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    background-color: #8d06e7;
+    color: white;
+  }
+`
 
 export default function HeaderButton({ children }) {
 
   return (
     <li>
-      <button className="tab-button">
+      <Button>
         {children}
-      </button>
+      </Button>
     </li>
   );
 }
