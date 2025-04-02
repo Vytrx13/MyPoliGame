216fdcae92c64c64ecc20a6ff6e1d149be6ae057
Vinicius Terra <vinicius13terra@gmail.com>
2025-04-02 13:41:04 -0300

    ao adicionar um jogo q ja esta na sua lista , o rating e o tipo da lista sao atualizados

--- backend/routes/listas.js
+++ backend/routes/listas.js
@@ -3,21 +3,43 @@ const express = require('express');
 
 const router = express.Router();
 
+async function getRegistroId(username, gameId) {
+    try {
+        const result = await pool.query('SELECT * FROM registro_lista WHERE username = $1 AND jogo_id = $2', [username, gameId]);
+        // console.log(result.rows[0]);
+        if (result.rows[0] !== undefined)
+            return result.rows[0].id;
+        return 0; 
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 router.post('/add', async (req, res) => {
     const {user, selectedList, gameId, gameName, imageUrl, score} = req.body;
     console.log(user, selectedList, gameId, gameName, imageUrl, score);
-
-    try {
-        const query = 'INSERT INTO registro_lista (username, tipo, jogo_id, nome_jogo, url_imagem, rating) VALUES ($1, $2, $3, $4, $5, $6)';
-        const values = [user, selectedList, gameId, gameName, imageUrl, score];
-        await pool.query(query, values);
-        res.status(201).json({ message: 'Jogo adicionado com sucesso!' });
-    } catch (err) {
-        console.error('Erro ao adicionar jogo:', err);
-        res.status(500).json({ error: 'Erro ao adicionar jogo' });
+    const registro_id = await getRegistroId(user, gameId); // retorna zero se n existe
+    console.log(registro_id);
+    if (registro_id !== 0) {
+        try {
+        await pool.query("UPDATE registro_lista SET tipo = $1, rating = $2 WHERE id = $3", [selectedList, score, registro_id]);
+        res.status(201).json({ message: 'Lista atualizada com sucesso!' });
+        } catch (err) {
+            console.error('Erro ao adicionar jogo:', err);
+            res.status(500).json({ error: 'Erro ao atualizar lista' });
+        }
     }
-
+    else {
+        try {
+            const query = 'INSERT INTO registro_lista (username, tipo, jogo_id, nome_jogo, url_imagem, rating) VALUES ($1, $2, $3, $4, $5, $6)';
+            const values = [user, selectedList, gameId, gameName, imageUrl, score];
+            await pool.query(query, values);
+            res.status(201).json({ message: 'Jogo adicionado com sucesso!' });
+        } catch (err) {
+            console.error('Erro ao adicionar jogo:', err);
+            res.status(500).json({ error: 'Erro ao adicionar jogo' });
+        }
+    }
 
 })
 
