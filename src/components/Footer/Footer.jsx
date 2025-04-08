def6e471b742b92fa015f403b458ada4cbe6b5d0
Vinicius Yshimine Terra <vinicius13terra@gmail.com>
2025-04-08 07:56:49 -0300

    tirei placeholder

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
     <p>&copy; 2025, Gangue da Politécnica</p>
-    <p>Feito com <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Placeholder</a></p>
+    <p>Feito com <a href="https://www.giantbomb.com/api/" target="_blank">Giant Bomb API</a></p>
     </footer>
   );
 }
